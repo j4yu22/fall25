4682db0aee49e7de5a60a7eaf774c300e91af297
j4yu22 <jay.e.underwood@gmail.com>
2025-09-20 12:05:59 -0600

    added git ignore and hello world

---
+++ encDes/HelloWorld/HelloWorld.cpp
@@ -0,0 +1,26 @@
+/*************************************************************
+ * 1. Name:
+ *      Jay Underwood
+ * 2. Assignment Name:
+ *      Lab 01: Hello World
+ * 3. Assignment Description:
+ *      A simple program to display a message on the screen
+ * 4. What was the hardest part? Be as specific as possible.
+ *      It was pretty easy, but I had to look up some sytax stuff, and getting the environment took some time.
+ * 5. How long did it take for you to complete the assignment?
+ *      1 hour
+ *****************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+/*
+	This is a block comment.
+*/
+
+int main() {
+	// This is a line comment
+	cout << "Hello world." << endl;
+	return 0;
+}
